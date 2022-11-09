172372a3ff7e9632e4fab05e9c84c37dd50196f5
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2022-11-09 10:20:27 -0300

    fix rural supervisor, order support

--- components/Forms/SelectWorker.vue
+++ components/Forms/SelectWorker.vue
@@ -78,7 +78,7 @@ export default {
     index: {
       type: Number,
       default: null,
-    },
+    }
   },
   data() {
     return {
@@ -119,11 +119,7 @@ export default {
         params.campaign_cycle_id = this.campaignCycleId;
 
         if (this.selectedWorker === null) {
-          if (this.listType === 'rural_supervisors') {
-            params.list_type = this.listType;
-          } else {
-            params.list_type = 'free';
-          }
+          params.list_type = 'free';
         } else if (typeof this.selectedWorker === 'number') {
           params.list_type = this.listType;
           params.coordinator_id = this.selectedWorker;
@@ -136,7 +132,7 @@ export default {
               params.ids.push(worker.id);
             }
           } else {
-            params.list_type = 'free';
+            params.list_type = this.listType;
           }
         }
       } else {
